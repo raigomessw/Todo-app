6599c7429403c94bd702dfcdda546977290f541e
Rai Gomes <raibr16@gmail.com>
2024-04-24 20:20:24 +0200

    Upp color

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,43 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./header.css";
 
 const Header = () => {
   return (
-    <header style={headerStyle}>
-      <h1 style={titleStyle}>Welcome to my todo app😁</h1>
-          <div style={links}>
-              <Link to="/">Welcome</Link>
-        <Link to="/TodoApp">TodoApp</Link>
-        <Link to="/about">About</Link>
+    <header className="header">
+      <h1 className="title">Welcome to my todo app😁</h1>
+      <div className="links">
+        <Link className="link-welcome" to="/">
+          Welcome
+        </Link>
+        <Link className="link-todoapp" to="/TodoApp">
+          TodoApp
+        </Link>
+        <Link className="link-about" to="/about">
+          About
+        </Link>
       </div>
     </header>
   );
-};
-
-// Styles
-const headerStyle = {
-  color: "#white",
-  padding: "20px",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-around",
-};
-
-const links = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  gap: "20px",
-  color: "white",
-  textDecoration: "none",
-};
-
-const titleStyle = {
-  fontSize: "2rem",
-  margin: "0",
-  alignItems: "center",
-  display: "flex",
 };
 
 export default Header;
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,7 +7,11 @@ const Welcome = () => {
       <h1>Välkommen till Todo-appen!</h1>
       <p>Här kan du hantera dina uppgifter och få saker gjorda.</p>
       <p>Starta med att skapa din egen lista.</p>
+      <div className='welcome-img'>
+        <img src="https://as2.ftcdn.net/v2/jpg/04/59/86/27/1000_F_459862700_blwVQ5me7hzE64Rfe40hX1yycwipwRw1.jpg" alt="Girl in a jacket" ></img>
+      </div>
     </div>
+    
   )
 }
 
